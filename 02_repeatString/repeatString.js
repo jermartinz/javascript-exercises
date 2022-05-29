19ed0769714e78cae1e33210114224e0ec421db6
Jeremy <jeremyastudillo19@outlook.com>
2022-05-28 20:42:07 -0400

    exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    string = '';
+    if(num < 0){
+        return 'ERROR';
+    }
+    if(num === 0){
+        return '';
+    }
+    for(let i = 0; i < num; i++){
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...position) {
+    position = position.map(Number);
+    let index = array.indexOf(position);
+    if(index > -1){
+        array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
