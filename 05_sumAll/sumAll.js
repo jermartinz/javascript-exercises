34477986e1a666228db892e0104293713b69cdca
Jeremy <61643086+jermartinz@users.noreply.github.com>
2022-06-11 20:10:50 -0400

    exercise complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(numMin, numMax) {
+    if(!Number.isInteger(numMin) || !Number.isInteger(numMax)){
+        return 'ERROR'
+    }
+    if(numMin <= 0 || numMax <= 0){
+        return 'ERROR'
+    }
+    if (numMin > numMax){
+        const numTemp = numMin;
+        numMin = numMax;
+        numMax = numTemp;
+    }
+    let sum = 0;
+    for(let i = numMin; i <= numMax; i++){
+        sum += i;
+    }
+    return sum;
 
 };
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
